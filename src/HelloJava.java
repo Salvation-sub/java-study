00f0d87f5706bfe2efc0d93fdb683f75de0f533c
Salvation_sub <kyman19940214@gmail.com>
2025-06-06 11:03:45 +0900

    java basic

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+// Class는 파일명과 같아야한다.
+public class HelloJava {
+    // main은 메서드로 구분된다 class >> method
+    public static void main(String[] arg){
+        System.out.println("Hello Java");
+        System.out.println("Hello Java 2");
+        System.out.println("Hello Java 3");
+    }
+}
