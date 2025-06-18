38ffbbaa60f7d63a4a7496f03816965854db0c40
Salvation-sub <kyman19940214@gmail.com>
2025-06-18 18:10:52 +0900

    feat: package

---
+++ src/package1/Data.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class Data {
+    public Data() {
+        System.out.println("패키지 package1 Data 생성");
+    }
+}
---
+++ src/package1/a/User2.java
@@ -0,0 +1,8 @@
+package package1.a;
+
+public class User2 {
+
+    public User2(){
+        System.out.println("패키지 package1.a 회원 생성");
+    }
+}
