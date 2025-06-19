20e250492b0f2385d0c9c23dc3d771f1a0d23ba7
Salvation-sub <kyman19940214@gmail.com>
2025-06-19 17:58:00 +0900

    feat: access 접근 제어자

---
+++ src/access/SpeakerMain.java
@@ -0,0 +1,18 @@
+package access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolume();
+        speaker.volumeUp();
+        speaker.showVolume();
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        //필드에 직접 접근해서 설정해둔 최대 volume을 수정 가능
+        System.out.println("volume 필드 직접 접근 수정");
+        speaker.volume = 200;
+        speaker.showVolume();
+    }
+
+}
---
+++ src/access/a/PublicClass.java
@@ -0,0 +1,18 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass pubClass = new PublicClass();
+        DefaultClass1 defClass1 = new DefaultClass1();
+        DefaultClass2 defClass2 = new DefaultClass2();
+    }
+}
+
+
+class DefaultClass1 {
+
+}
+
+class DefaultClass2 {
+
+}
