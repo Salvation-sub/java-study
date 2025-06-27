ebacdacbcfec37ff786cc8aa2cb11c206545adbd
Salvation-sub <kyman19940214@gmail.com>
2025-06-27 10:09:18 +0900

    feat: 다형성 다운캐스팅, 업캐스팅

--- src/poly/basic/PolyMain.java
+++ src/poly/basic/PolyMain.java
@@ -12,5 +12,14 @@ public class PolyMain {
         Child child = new Child();
         child.parentMethod();
         child.childMethod();
+
+        // 부모 변수가 자식 인스턴스 참조(다형적 참조)
+        System.out.println("Parent -> Child");
+        // 부모타입은 자식 타입을 담을 수 있다.
+        Parent poly = new Child();
+        poly.parentMethod();
+
+        // 자식 타입은 부모를 담을수 없다.
+//        Child child1 = new Parent();
     }
 }
